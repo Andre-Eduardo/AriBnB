2162969b5a94ef91b055c1ec7e42bbb9fab0227f
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-10-08 10:55:22 -0300

    adicionando metodos de requisiçao no imovel

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const Property = use('App/Models/Property')
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
@@ -18,8 +18,12 @@ class PropertyController {
    * @param {View} ctx.view
    */
   async index ({ request, response, view }) {
+    const properties = Property.all()
+
+  return properties
   }
 
+  
   /**
    * Render a form to be used for creating a new property.
    * GET properties/create
@@ -29,17 +33,7 @@ class PropertyController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new property.
-   * POST properties
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
+  
   async store ({ request, response }) {
   }
 
@@ -53,6 +47,11 @@ class PropertyController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+  
+    return property
   }
 
   /**
@@ -64,17 +63,7 @@ class PropertyController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update property details.
-   * PUT or PATCH properties/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
+  
   async update ({ params, request, response }) {
   }
 
@@ -87,6 +76,13 @@ class PropertyController {
    * @param {Response} ctx.response
    */
   async destroy ({ params, request, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    if (property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+  
+    await property.delete()
   }
 }
 
